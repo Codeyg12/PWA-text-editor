dacf15c8e8232ee13a1aa2414b44ead5dc4ae29e
Codeyg12 <codey.gallup@gmail.com>
2022-10-08 12:03:36 -0600

    updating

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -15,23 +17,23 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Just Another Text Editor'
+        title: 'JATE'
       }),
-
+  
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js'
       }),
-
+  
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
-        description: 'just another text editor',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor like others',
         background_color: '#00beef',
         theme_color: '#111893',
         start_url: './',
@@ -40,13 +42,12 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons')
+            destination: path.join('assets', 'icons'),
           }
         ]
       })
     ],
-    
-    // TODO: Add CSS loaders and babel to webpack.
+  
     module: {
       rules: [
         {
@@ -55,16 +56,16 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime']
-            }
-          }
-        }
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
-};
+  };
